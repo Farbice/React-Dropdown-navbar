aaf7d89c0af49fbaaad6cdfd0d8c290079cf68b5
Farbice <109209812+Farbice@users.noreply.github.com>
2023-09-26 23:59:24 +0200

    Navlinks + burger menu add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
